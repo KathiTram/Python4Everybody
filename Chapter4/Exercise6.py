a55e93ea7f84a3b85ac37a3e06bc1330c2acda29
KathiTram <49622903+KathiTram@users.noreply.github.com>
2022-05-17 09:47:05 -0700

    Create Exercise6.py

---
+++ Chapter4/Exercise6.py
@@ -0,0 +1,23 @@
+# Exercise 6: Rewrite your pay computation with time-and-a-half for overtime and create a function called computepay which takes two parameters (hours and rate).
+
+def computepay(hours, rate):
+    try:
+      # hours = int(input("Hours: "))
+      # rate = float(input("Rate: "))
+
+      if hours > 40:
+        overtimeHours = hours - 40
+        overtimePay = overtimeHours * 1.5 * rate
+        regularPay = 40 * rate
+        totalPay = overtimePay + regularPay
+        print("Your overtime pay is: " + str(overtimePay))
+        print("Your regular pay is: " + str(regularPay))
+        print("Your total pay is: " + str(totalPay))
+      else:
+        pay = hours * rate
+        print("Your pay is: " + str(pay))
+    except:
+      print("Error, please enter a numeric input")
+
+print("Enter your hours and rate:\n")
+computepay(int(input()), float(input()))
